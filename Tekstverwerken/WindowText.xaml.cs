dcfc431ce88858db218ddd8ae92e06a1f99ef757
kurtkreemers <kurt.kreemers@student.pxl.be>
2015-04-21 14:25:58 +0200

    oefening

---
+++ Tekstverwerken/WindowText.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TekstVerwerken
+{
+    /// <summary>
+    /// Interaction logic for WindowText.xaml
+    /// </summary>
+    public partial class WindowText : Window
+    {
+        public WindowText()
+        {
+            
+        }
+
+        private void knopklik(object sender, RoutedEventArgs e)
+        {
+            textBlockAanmelding.TextWrapping = TextWrapping.Wrap;
+            textBlockAanmelding.Text = "Je probeerde aan te melden met: " + textBoxGebruikersnaam.Text + " en paswoord: " + psdBox.Password;
+        }
+
+       
+
+
+        
+    }
+}
