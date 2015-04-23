9c663dfe57e4a6934c5f91d58b92a939b15dd910
kurtkreemers <kurt.kreemers@student.pxl.be>
2015-04-23 16:03:09 +0200

    23042015

---
+++ Telefoon/TelefoonWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Telefoon
+{
+    /// <summary>
+    /// Interaction logic for TelefoonWindow.xaml
+    /// </summary>
+    public partial class TelefoonWindow : Window
+    {
+        public TelefoonWindow()
+        {
+            InitializeComponent();
+        }
+        public List<Persoon> personen = new List<Persoon>; 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboBoxSelectie.Items.Add("Iedereen");
+            ComboBoxSelectie.Items.Add("Familie");
+            ComboBoxSelectie.Items.Add("Vrienden");
+            ComboBoxSelectie.Items.Add("Werk");
+            ComboBoxSelectie.SelectedIndex = 0;
+        }
+    }
+}
---
+++ WpfCursus/Hobby.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media.Imaging;
+namespace WpfCursus
+{
+    public class Hobby
+    {
+        public string Categorie { get; set; }
+        public string Activiteit { get; set; }
+        public BitmapImage Symbool { get; set; }
+
+        public Hobby(string nCategorie, string nActiviteit, BitmapImage nSymbool)
+        {
+            Categorie = nCategorie;
+            Activiteit = nActiviteit;
+            Symbool = nSymbool;
+        }
+    }
+}
